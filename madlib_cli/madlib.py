36b6fcda453c8fee369e96d0b814c14e30328216
Logan Jones <loganemilyjones@mac.com>
2020-12-12 15:59:47 -0800

    User can enter custom words

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,11 +1,13 @@
 import re
 
 welcome_text = """
-*******************
+*************************************************************************
 
 Welcome to MadLibs!
 
-*******************
+Provide creative inputs to build your own story. Enter 'quit' at anytime.
+
+*************************************************************************
 """
 
 
@@ -13,14 +15,22 @@ def welcome():
     print(welcome_text)
 
 
+def word_prompt(word_type):
+    prompt = f"\nEnter a(n) {word_type}:\n> "
+    return prompt
+   
+
 def read_template(path: str):
-    with open(path, 'r') as file:
-        return file.read()
-    
+    try:
+        with open(path, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        raise
+
 
 def parse_template(string: str):
 
-    regex = r"{([a-zA-z]*)}"
+    regex = r"{([^}]*)}"
     subst = "{}"
 
     parts = tuple(re.findall(regex, string))
@@ -39,3 +49,25 @@ def merge(string: str, words: tuple):
     
     return string
 
+
+def madlibs():
+    
+    template = read_template('../assets/template.txt')
+    stripped, parts = parse_template(template)
+
+    new_words = []
+
+    for part in parts:
+        new_word = input(word_prompt(part))
+        if new_word == 'quit':
+            quit()
+        else:
+            new_words.append(new_word)
+    
+    new = merge(stripped, tuple(new_words))
+    print(new)
+
+
+# Run it
+welcome()
+madlibs()
